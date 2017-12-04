6833d080f403c61d0394b8e7293497352e8604b6
Victor Niu <bniu2@illinois.edu>
2017-12-04 00:02:23 -0600

    add constants

---
+++ pivots/constants.go
@@ -0,0 +1,22 @@
+package pivots
+
+//Directional Constants
+const (
+	North = iota
+	East
+	South
+	West
+	Up
+	Down
+)
+
+//X, Y, and Z positioning
+const (
+	X = iota
+	Y
+	Z
+)
+
+const (
+	Look = 99
+)
